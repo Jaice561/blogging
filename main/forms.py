7859a62f20e38b13b61bacae26d564d36996973a
Jaice561 <Jameson_exil@yahoo.com>
2020-08-19 14:48:34 -0500

    full crud, links are connected, with some css, with additional model

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+from .models import blogPost
 
 class ContactForm(forms.Form):
     full_name = forms.CharField()
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
+
+# class blogPostForm(forms.Form):
+#     title = forms.CharField()
+#     slug = forms.SlugField()
+#     content = forms.CharField(widget=forms.Textarea)
+    
+class blogPostModelForm(forms.ModelForm):
+    class Meta:
+        model = blogPost
+        fields = ['title', 'slug', 'content']
+
+    def clean_title(self, *args, **kwargs):
+        instance = self.instance
+        title = self.cleaned_data.get('title')
+        qs = blogPost.objects.filter(title__iexact=title)
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk)
+        if qs.exists():
+            raise forms.ValidationError("This title has already been used. Please try again")
+        return title
--- main/views.py
+++ main/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import blogPost
 from .forms import ContactForm
+from .forms import blogPostModelForm
 # Create your views here.
+
 def blog_list_view(request):
-    qs = blogPost.objects.all()
+    qs = blogPost.objects.all().published()
     template_name = 'blog_list.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
 
+@login_required(login_url='/login')
+@staff_member_required
 def blog_create_view(request):
-    template_name = 'blog_create.html'
-    context = {"form": ''}
+    form = blogPostModelForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.user = request.user
+        obj.save()
+        form = blogPostModelForm()
+    template_name = 'form.html'
+    context = {"form": form}
     return render(request, template_name, context)
 
 def blog_detail_view(request, slug):
@@ -19,23 +31,32 @@ def blog_detail_view(request, slug):
     context = {"object": obj}
     return render(request, template_name, context)
 
-def blog_update_view(request):
+@login_required(login_url='/login')
+@staff_member_required
+def blog_update_view(request, slug):
     obj = get_object_or_404(blogPost, slug=slug)
-    template_name = 'blog_update.html'
-    context = {"object": obj, 'form': ''}
+    form = blogPostModelForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    template_name = 'form.html'
+    context = {'form': form, "title": f"Update {obj.title}"}
     return render(request, template_name, context)
 
-def blog_delete_view(request):
+@login_required(login_url='/login')
+@staff_member_required
+def blog_delete_view(request, slug):
     obj = get_object_or_404(blogPost, slug=slug)
     template_name = 'blog_delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/")
     context = {"object": obj}
     return render(request, template_name, context)
 
 def home(request):
     title = "Hello there...."
-    context = {"title": title}
-    if request.user.is_authenticated:
-        context = {"title": title, "list": [1,2,3,4,5]}
+    qs = blogPost.objects.all()[:5]
+    context = {"title": "Welcome to my blog website", "blog_list": qs}
     return render(request, "home.html", context)
 
 def about(request):
